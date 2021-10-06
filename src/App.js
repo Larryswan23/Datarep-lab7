37070567a6389d5f0b90898c39fb69c5283fd653
Martin Kenirons <martin.kenirons@gmit.ie>
2021-10-06 11:35:24 +0100

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/footer">Footer</Nav.Link>
+              <Nav.Link href="/header">Header</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Switch>
+          <Route path="/" exact><Content /></Route>
+          <Route path="/header"><Header></Header></Route>
+          <Route path="/footer"><Footer></Footer></Route>
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+export default Content;
