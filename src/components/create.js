3f734697e48794375c6392128ae49129be456872
Martin Kenirons <martin.kenirons@gmit.ie>
2021-10-19 10:35:51 +0100

    lab 4 complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react';
 
-class Create extends Component
-{
-    render(){
-        return(
+class Create extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeMovieYear = this.onChangeMovieYear.bind(this);
+        this.onChangeMoviePoster = this.onChangeMoviePoster.bind(this);
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    handleSubmit(event) {
+        console.log("Name: " +this.state.Title+
+        " Year: " + this.state.Year +
+        "Poster: " + this.state.Poster);
+        event.preventDefault();
+        this.setState({
+            Title:'',
+            Year:'',
+            Poster:''
+        });
+    }
+    onChangeMovieName(event) {
+        this.setState({
+            Title: event.target.value
+        })
+    }
+    onChangeMovieYear(event) {
+        this.setState({
+            Year: event.target.value
+        })
+    }
+    onChangeMoviePoster(event){
+        this.setState({
+            Poster: event.target.value
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <h1>This is my Create Component!</h1>
+                <form onSubmit={this.handleSubmit}>
+
+                    <div className="form-group">
+                        <label>Add Movie Name: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Title}
+                            onChange={this.onChangeMovieName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.Year}
+                            onChange={this.onChangeMovieYear}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Add Movie Poster: </label>
+                        <textarea type="text"
+                            className="form-control"
+                            value={this.state.Poster}
+                            onChange={this.onChangeMoviePoster}
+                        />
+                    </div>
+                    <div>
+                        <input type="submit" value="Add Movie"
+                            className="btn btn-primary"></input>
+                    </div>
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
 import Movies from './movies';
-
+import axios from 'axios';
 
 class Read extends Component
 {
+
+    componentDidMount(){
+        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+        .then((response)=>{
+            this.setState({mymovies: response.data.movies})
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    }
+
     state = {
-        mymovies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]            
+        mymovies: []            
     };
 
     render(){
